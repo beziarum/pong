eb8b8157c8a413f5b3721d9b1d12655c773d3e1b
Raitei3 <antoine24.borde@gmail.com>
2015-11-13 10:44:16 +0100

    ajout du package game

--- game/Game.java
+++ game/Game.java
@@ -13,12 +13,12 @@ public class Game {
 	
 	public void limit(){
 		if (i.getPosition().y < 0)
-			i.rebondir(Direction.haut);
+			i.rebondir(Direction.haut,SIZE_PONG_X,SIZE_PONG_Y);
 		if(i.getPosition().y > SIZE_PONG_Y - i.getHeight()/2)
-			i.rebondir(Direction.bas);
+			i.rebondir(Direction.bas,SIZE_PONG_X,SIZE_PONG_Y);
 		if(i.getPosition().x > SIZE_PONG_X - i.getWidth()/2)
-			i.rebondir(Direction.gauche);
+			i.rebondir(Direction.gauche,SIZE_PONG_X,SIZE_PONG_Y);
 		if(i.getPosition().x < 0)
-			i.rebondir(Direction.droite);
+			i.rebondir(Direction.droite,SIZE_PONG_X,SIZE_PONG_Y);
 	}
 }
