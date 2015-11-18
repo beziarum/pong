1a82b0ee110c62c25f0d04676bf12ee17a14aaf7
paul <paul.beziau@laposte.net>
2015-11-18 15:36:32 +0100

    début de l'implémentation de la boucle de jeu dans la classe game

--- game/Game.java
+++ game/Game.java
@@ -1,5 +1,8 @@
 package game;
 
+import java.util.ArrayList;
+
+import pong.gui.Ball;
 import pong.gui.PongItem;
 import pong.gui.Racket;
 import pong.util.Direction;
@@ -9,9 +12,9 @@ public class Game {
 	int SIZE_PONG_X = 480;
 	int SIZE_PONG_Y = 480;
 	
-	public PongItem i;
 	
-	public void limit(){
+	private ArrayList<PongItem> a;
+	private void limit(PongItem i){
 		if (i.getPosition().y < 0)
 			i.rebondir(Direction.haut,SIZE_PONG_X,SIZE_PONG_Y);
 		if(i.getPosition().y > SIZE_PONG_Y - i.getHeight()/2)
@@ -20,5 +23,22 @@ public class Game {
 			i.rebondir(Direction.gauche,SIZE_PONG_X,SIZE_PONG_Y);
 		if(i.getPosition().x < 0)
 			i.rebondir(Direction.droite,SIZE_PONG_X,SIZE_PONG_Y);
+	}
+	
+	Game()
+	{
+		a.add(new Racket());
+		a.add(new Ball());
+	}
+	
+	public void run()
+	{
+		while(true)
+		{
+			for (PongItem e : a) {
+				limit(e);
+				e.animate();
+			}
+		}
 	}
 }
