cf303a799b83cd55b5d3bf073559f13963385d96
paul <paul.beziau@laposte.net>
2015-11-18 15:03:03 +0100

    ajout des fonction nécessaires à Ball.java (rebondir notamment)

--- pong/gui/Ball.java
+++ pong/gui/Ball.java
@@ -5,6 +5,8 @@ import java.awt.*;
 import javax.swing.JPanel;
 import javax.swing.ImageIcon;
 
+import pong.util.Direction;
+
 public class Ball extends PongItem {
     
     protected static final int INIT_SPEED = 4;
@@ -16,5 +18,12 @@ public class Ball extends PongItem {
 	speed = new Point(INIT_SPEED,INIT_SPEED);
     }
 
-    
+    public void rebondir(Direction d,int window_width,int window_height)
+    {
+    	if(d==Direction.haut || d==Direction.bas)
+    		speed.y=0-speed.y;
+    	else if(d==Direction.droite || d==Direction.gauche)
+    		speed.x=0-speed.x;
+    	
+    }
 }
