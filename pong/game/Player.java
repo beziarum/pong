6d57c0b32514edbdd02b6ea6308f16377357dd30
paul <paul.beziau@laposte.net>
2015-12-02 12:21:51 +0100

    Continuation de l'interface réseau

--- pong/game/Player.java
+++ pong/game/Player.java
@@ -1,22 +1,41 @@
 package pong.game;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
 
 import pong.gui.Bordure;
 import pong.gui.Racket;
 
 public class Player {
-	Racket racket;
-	Bordure b;
+	private Socket s;
+	private DataInputStream is;
+	private Racket racket;
+	private Bordure b;
 	
 	Player(Socket s,Bordure b)
 	{
 		this.b=b;
 		racket=new Racket();
+		this.s=s;
+		try {
+			is=new DataInputStream(s.getInputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public Racket getRacket()
 	{
+		String s=new String();
+		char c='b';
+		while(c!='\n')
+		{
+			c=is.readChar();
+			s.concat(c);
+		}
+		
 		return racket;
 	}
 	
@@ -27,6 +46,7 @@ public class Player {
 	
 	public boolean isInGameOver()
 	{
+		
 		return false;
 	}
 }
