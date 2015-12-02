a03aafaa1be22ce5127cb28027dc5d5dedec50cf
paul <paul.beziau@laposte.net>
2015-12-02 18:16:53 +0100

    continuation de la classe player, les fonctions getRacket et sendNewPos doivent normalement marcher

--- pong/game/Player.java
+++ pong/game/Player.java
@@ -1,8 +1,10 @@
 package pong.game;
 
+import java.awt.Point;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 import pong.gui.Bordure;
@@ -11,6 +13,7 @@ import pong.gui.Racket;
 public class Player {
 	private Socket s;
 	private DataInputStream is;
+	private OutputStream os;
 	private Racket racket;
 	private Bordure b;
 	
@@ -21,6 +24,7 @@ public class Player {
 		this.s=s;
 		try {
 			is=new DataInputStream(s.getInputStream());
+			os=s.getOutputStream();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -43,12 +47,23 @@ public class Player {
 		String[] worlds=s.toString().split(" ");
 		if(worlds.length != 2)
 			throw new RuntimeException("Unexpected protocol");
+		racket.setPosition(Integer.valueOf(worlds[0]),Integer.valueOf(worlds[1]));
 		return racket;
 	}
 	
-	public void sendNewPos()
+	public void sendNewPos(Point pos)
 	{
-		
+		try {
+			StringBuffer sb=new StringBuffer();
+			sb.append(pos.x);
+			sb.append(' ');
+			sb.append(pos.y);
+			sb.append('\n');
+			os.write(sb.toString().getBytes());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public boolean isInGameOver()
