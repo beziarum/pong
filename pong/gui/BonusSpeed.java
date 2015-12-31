ed405449ebb442c5ae25aec0484e8c766e4af102
Raitei3 <antoine24.borde@gmail.com>
2015-12-31 17:03:10 +0100

    fin de la classe bonus speed

--- pong/gui/BonusSpeed.java
+++ pong/gui/BonusSpeed.java
@@ -11,21 +11,78 @@ public class BonusSpeed extends PongItem{
 
 	protected static final int INIT_SPEED= 0;
 	
-	protected static Point INIT_POSITION = RandomNumber.randomPoint((Window.WINDOW_SIZE.width/2)-100,(Window.WINDOW_SIZE.width/2)+100,(Window.WINDOW_SIZE.height/2)-100,(Window.WINDOW_SIZE.height/2)+100);//a vérifier
+	protected static final double boost= 1.5;
+	protected static final int delay= 10000;
+	protected static Ball ball;
+	protected static long timer;
+	protected static boolean efficient;
+	protected static boolean spawner;
 	
+	protected static Point INIT_POSITION = new Point(-100,-100);
 	
-	public BonusSpeed()
+	/*RandomNumber.randomPoint((Window.WINDOW_SIZE.width/2)-100,(Window.WINDOW_SIZE.width/2)+100,(Window.WINDOW_SIZE.height/2)-100,(Window.WINDOW_SIZE.height/2)+100);//a vérifier*/
+	
+	public BonusSpeed(Ball b)
     {	
     	super("image/boostSpeed.png", INIT_POSITION,new Point(INIT_SPEED ,INIT_SPEED));
+    	ball = b;
+    	timer = 0;
+    	efficient = false;
+    	spawner = false;
     }
 
 
 	public void rebondir(Direction d, int window_width, int window_height) {
 	}
 	
-	public static boolean spawn(){
+	public boolean spawn(){
 		int x = RandomNumber.randomValue(0, 180);//a amélioré
-		return (x<=1);
+		return (x<=1);	
+	}
+	
+	public void spawning(){
+		this.position = RandomNumber.randomPoint((Window.WINDOW_SIZE.width/2)-100,(Window.WINDOW_SIZE.width/2)+100,(Window.WINDOW_SIZE.height/2)-100,(Window.WINDOW_SIZE.height/2)+100);//a vérifier*/
+		spawner = true;
+	}
+	
+	
+	public void process(){
+		if (efficient)
+		{
+			if ((System.currentTimeMillis()-timer)>=10000)
+			{
+				desactivation();
+			}
+		}
+		else{
+			if (ball.collision(this)!=Direction.aucune)
+			{
+				activation();
+			}
+			else if(!spawner)
+			{
+				if (spawn())
+					spawning();
+			}
+		}
+	}
+	
+	public void activation(){
+		Point pnew = (Point) ball.getSpeed().clone();
+		pnew.setLocation(getX()*1.5, getY()*1.5);
+		ball.setSpeed(pnew);
+		efficient = true;
+		timer = System.currentTimeMillis();
+		this.position= INIT_POSITION;
+		spawner = false;
+	}
+	
+	public void desactivation(){
+		Point pold = (Point) ball.getSpeed().clone();
+		pold.setLocation(getX()/1.5, getY()/1.5);
+		ball.setSpeed(pold);
+		efficient = false;
+		timer=0;
 	}
 	
 	
