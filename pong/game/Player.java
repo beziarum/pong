c302650f75a57865222a0e49bd6b4518133d5fb4
paul <paul.beziau@laposte.net>
2016-01-04 02:02:51 +0100

    Revert "suppression des syso de débugs"
    This reverts commit b28956fcbb83270c1ed5ca1e217b513c87946ba1.

--- pong/game/Player.java
+++ pong/game/Player.java
@@ -45,15 +45,15 @@ public class Player {
 		{
 			NetworkControler.sendRacket(os, r);
 			NetworkControler.sendBall(os, b);
-			NetworkControler.nextData(is);
+			System.out.println("  --  "+NetworkControler.nextData(is));
 			NetworkControler.readRacket(is, racket);
 		}
 		else
 		{
 			NetworkControler.sendRacket(os, r);
-			NetworkControler.nextData(is);
+			System.out.println("  --  "+NetworkControler.nextData(is));
 			NetworkControler.readRacket(is, racket);
-			NetworkControler.nextData(is);
+			System.out.println("  --  "+NetworkControler.nextData(is));
 			NetworkControler.readBall(is, b);
 		}
 	}
