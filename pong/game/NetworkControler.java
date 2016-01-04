81b5b7d7b76cea3e9a6a0c53c9474e8aa9847837
paul <paul.beziau@laposte.net>
2016-01-04 02:02:57 +0100

    Revert "correction mineures dans le protocole"
    This reverts commit 993832f0952c014f9f4d0e984a7317ffdf9bb71e.

--- pong/game/NetworkControler.java
+++ pong/game/NetworkControler.java
@@ -208,7 +208,7 @@ public class NetworkControler {
 		StringBuffer ret=new StringBuffer();
 		try{
 		for(int i=0; i<4; i++)
-			ret.append((char)is.read());
+			ret.append(is.read());
 		} catch(SocketException e){
 			throw e;
 		} catch(IOException e){
