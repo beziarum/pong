b9663c7ddf7e2f37c93dccb7a8ff2d48b53b5d89
paul <paul.beziau@laposte.net>
2016-01-04 03:10:06 +0100

    commentaires de la classe Player

--- pong/game/Player.java
+++ pong/game/Player.java
@@ -1,8 +1,8 @@
 package pong.game;
 
-import java.io.DataInputStream;
 import java.io.EOFException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.SocketException;
@@ -12,49 +12,103 @@ import pong.gui.Bordure;
 import pong.gui.Racket;
 import pong.util.Direction;
 
+/**
+ * La classe Player gère un joueur distant
+ * @author paul et antoine
+ *
+ */
 public class Player {
-	private DataInputStream is;
+	/**
+	 * InputStream vers ce joueur
+	 */
+	private InputStream is;
+	
+	/**
+	 * OutputStream vers ce joueur
+	 */
 	private OutputStream os;
+	
+	/**
+	 * Raquette gérée par ce joueur
+	 */
 	private Racket racket;
+	
+	/**
+	 * Bordure liée à ce joueur
+	 */
 	private Bordure bordure;
+	
+	/**
+	 * score de ce joueur
+	 */
 	private int score;
 	
+	/**
+	 * Constructeur de la classe Player
+	 * @param s Socket vers ce joueur
+	 * @param b Bordure lié à ce joueur
+	 */
 	Player(Socket s,Bordure b)
 	{
 		score=0;
 		bordure=b;
 		racket=new Racket();
 		try {
-			is=new DataInputStream(s.getInputStream());
+			is=s.getInputStream();
 			os=s.getOutputStream();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
+	/**
+	 * @return La raquette liée à ce joueur
+	 */
 	public Racket getRacket()
 	{
 		return racket;
 	}
+	
+	/**
+	 * met à jour la position de la raquette de ce joueur
+	 * @throws EOFException
+	 * @throws SocketException
+	 */
 	public void updatePos() throws EOFException, SocketException
 	{
 		racket.setCenter(NetworkControler.readPos(is));
 	}
 	
+	/**
+	 * envoi au jouer la position de notre raquette
+	 * @param r notre raquette
+	 * @throws SocketException
+	 */
 	public void sendNewPos(Racket r) throws SocketException
 	{
 		NetworkControler.sendPos(os, r.getCenter());
 	}
 	
+	/**
+	 * @param ball la balle du pong
+	 * @return vrai si ce joueur est en situation de gameover, faux sinon
+	 */
 	public boolean isInGameOver(Ball ball)
 	{
 		return bordure.collision(ball)!=Direction.aucune;
 	}
 	
+	/**
+	 * @return le score du joueur
+	 */
 	public int getScore(){
 		return score;
 	}
 	
+	/**
+	 * met à jour le score du joueur
+	 * @param s le nouveau score du joueur
+	 */
 	public void setScore(int s){
 		score = s;
 	}
