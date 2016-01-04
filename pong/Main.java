b82e1b446cd55ec7ad96247066d80ddc9862fb1e
paul <paul.beziau@laposte.net>
2016-01-04 04:57:30 +0100

    correction du main

--- pong/Main.java
+++ pong/Main.java
@@ -11,8 +11,6 @@ import pong.game.Game;
  */
 public class Main  {
 	public static void main(String[] args) {
-		if(System.console()!=null)
-		System.out.println(System.console().readLine());
 		Game game= new Game();
 		game.run();
 	}
